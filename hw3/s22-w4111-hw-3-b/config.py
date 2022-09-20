dfe3266e24bf8476885a8104a9f3136721207de2
Daoyang E <77170972+dajianjue1999@users.noreply.github.com>
2022-09-20 16:49:38 -0400

    Add files via upload

---
+++ hw3/s22-w4111-hw-3-b/config.py
@@ -0,0 +1,7 @@
+
+neo_url = 'neo4j+s://13f30c6c.databases.neo4j.io:7687'
+neo_auth = ("neo4j", "isZCQgcAMZ9UusAH_w-hoqAtB7TsjJnXiEwrvbuCbGA")
+
+
+mongodb_url = "mongodb+srv://de2418:Edy990127@aaaa.1raes.mongodb.net/test?retryWrites=true&w=majority"
+
